ea65030350a9b85ed08fdd5eb032193116c0ccd5
ghaniaridha <itsghaniakh@gmail.com>
2026-06-18 23:56:45 +0700

    first commit

---
+++ pengajuan_pra_riset.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+include "koneksi.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Permohonan Pra Riset</title>
+
+    <link rel="shortcut icon" href="images/Logo UINRIL(2).png" />
+    <link rel="stylesheet" href="style.css" media="screen" title="no title">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar">
+        <a href="#" class="navbar-logo">
+            <img src="images/AKADEMIK FST2.png" alt="navbar-logo">
+        </a>
+
+        <div class="navbar-nav">
+            <a href="mhs_dashboard.php">Beranda</a>
+            <a href="mhs_dashboard.php#services">Layanan Akademik</a>
+            <a href="mhs_dashboard.php#riwayat">Informasi</a>
+            <a href="mhs_riwayat.php">Riwayat Permohonan</a>
+        </div>
+
+        <div class="navbar-extra">
+            <div class="user-menu-container">
+                <?php
+                $namaLengkap = isset($_SESSION['nama_lengkap']) ? $_SESSION['nama_lengkap'] : 'Pengguna';
+                $idLogin = isset($_SESSION['nama']) ? $_SESSION['nama'] : '';
+                $role = isset($_SESSION['role']) ? ucwords($_SESSION['role']) : 'ROLE';
+
+                $inisial = '';
+                $namaParts = explode(' ', $namaLengkap);
+                if (!empty($namaParts)) {
+                    $inisial = strtoupper(substr($namaParts[0], 0, 1));
+                }
+                ?>
+                <button id="user-btn" class="user-btn">
+                    <span class="avatar-inisial"><?= htmlspecialchars($inisial) ?></span>
+                </button>
+                <div id="user-dropdown" class="dropdown-menu">
+                    <div class="user-info">
+                        <span class="user-name"><?= ($namaLengkap) ?></span>
+                        <span class="user-role"><?= $idLogin ?> - <?= $role ?></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <section id="daftar-surat" class="daftar-surat">
+        <div class="daftar-surat-header">
+            <h2>Permohonan Pra Riset</h2>
+        </div>
+    </section>
+
+</body>
+
+</html>
